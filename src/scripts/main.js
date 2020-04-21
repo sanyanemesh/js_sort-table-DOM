19fae0435d3d25fd38651ba98e492f52a7c6665a
sanyanemesh <sanya.nemesh@gmail.comgit config --global user.name sanyanemeshgit config --global user.email sanya.nemesh@gmail.com>
2020-04-21 15:09:20 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-console.log('hello mate academy!');
+function makeSortable(table) {
+  const thead = table.querySelector('thead');
+  const tbody = table.querySelector('tbody');
+
+  thead.addEventListener('click', e => {
+    const th = event.target.closest('th');
+
+    if (!th) {
+      return;
+    }
+
+    const column = th.cellIndex;
+    const rows = [...tbody.children];
+
+    rows.sort((a, b) => {
+      const valueA = a.cells[column].textContent;
+      const valueB = b.cells[column].textContent;
+
+      return valueA > valueB ? 1 : -1;
+    });
+
+    for (const row of rows) {
+      tbody.appendChild(row);
+    }
+  });
+}
+
+makeSortable(document.querySelector('table'));
